abaf7f16f0f50028fe2f709dea1b5b01e7940f30
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2002-03-29 16:28:54 +0000

    Module: revision id.
    --HG--
    extra : convert_revision : svn%3A778c30c8-61e0-0310-89d4-fe2f97a467b2/trunk%4094

--- UserTuple.py
+++ UserTuple.py
@@ -2,6 +2,10 @@
 A more or less complete user-defined wrapper around tuple objects.
 Adopted version of the standard library's UserList.
 """
+# $Id: UserTuple.py,v 1.5 2002/03/29 16:28:54 schwa Exp $
+
+# Ideas for future development:
+#   - Rewrite tests to use mock FTP sessions
 
 #XXX tuple instances (in Python 2.2) contain also:
 #   __class__, __delattr__, __getattribute__, __hash__, __new__,
