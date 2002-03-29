20db7135b1038f096f4e58be5c7ff771668cd979
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2002-03-29 18:34:38 +0000

    FTP: added some method heads with their docstrings from ftplib.FTP .
    --HG--
    extra : convert_revision : svn%3A778c30c8-61e0-0310-89d4-fe2f97a467b2/trunk%40101

--- _mock_ftplib.py
+++ _mock_ftplib.py
@@ -29,13 +29,83 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# $Id: _mock_ftplib.py,v 1.1 2002/03/29 16:31:20 schwa Exp $
+# $Id: _mock_ftplib.py,v 1.2 2002/03/29 18:34:38 schwa Exp $
 
 """
 This module implements a mock version of the standard libraries
-ftplib.py module.
+ftplib.py module. Some code is taken from there.
 
 Not all functionality is implemented, only that which is used to
 run the unit tests.
 """
+
+class FTP:
+    """
+    Mock implementation of ftplib.FTP . For information on mock
+    objects see http://www.mockobjects.com/ .
+    """
+    def __init__(self, host, user, password):
+        self._host = host
+        self._user = user
+        self._password = password
+
+    def voidresp(self):
+        """Expect a response beginning with '2'."""
+    
+    def sendcmd(self, cmd):
+        """Send a command and return the response."""
+    
+    def voidcmd(self, cmd):
+        """Send a command and expect a response beginning with '2'."""
+
+    def ntransfercmd(self, cmd, rest=None):
+        """Initiate a transfer over the data connection.
+
+        If the transfer is active, send a port command and the
+        transfer command, and accept the connection.  If the server is
+        passive, send a pasv command, connect to it, and start the
+        transfer command.  Either way, return the socket for the
+        connection and the expected size of the transfer.  The
+        expected size may be None if it could not be determined.
+
+        Optional `rest' argument can be a string that is sent as the
+        argument to a RESTART command.  This is essentially a server
+        marker used to tell the server to skip over any data up to the
+        given marker.
+        """
+
+    def transfercmd(self, cmd, rest=None):
+        """Like ntransfercmd() but returns only the socket."""
+        return self.ntransfercmd(cmd, rest)[0]
+
+    def dir(self, *args):
+        """List a directory in long form.
+        By default list current directory to stdout.
+        Optional last argument is callback function; all
+        non-empty arguments before it are concatenated to the
+        LIST command.  (This *should* only be used for a pathname.)"""
+
+    def rename(self, fromname, toname):
+        """Rename a file."""
+
+    def delete(self, filename):
+        """Delete a file."""
+
+    def cwd(self, dirname):
+        """Change to a directory."""
+
+    def mkd(self, dirname):
+        """Make a directory, return its full pathname."""
+
+    def rmd(self, dirname):
+        """Remove a directory."""
+
+    def pwd(self):
+        """Return current working directory."""
+
+    def quit(self):
+        """Quit, and close the connection."""
+
+    def close(self):
+        """Close the connection without assuming anything about it."""
 
