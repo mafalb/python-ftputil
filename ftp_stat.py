c7301faf3a994c8ce1122084eb7c21d2a3e505bf
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2003-03-16 19:20:54 +0000

    `stat` result class for ftputil.
    --HG--
    extra : convert_revision : svn%3A778c30c8-61e0-0310-89d4-fe2f97a467b2/trunk%40235

---
+++ ftp_stat.py
@@ -0,0 +1,90 @@
+# Copyright (C) 2002, Stefan Schwarzer
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# - Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# - Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# - Neither the name of the above author nor the names of the
+#   contributors to the software may be used to endorse or promote
+#   products derived from this software without specific prior written
+#   permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+ftp_stat.py - stat result class for ftputil
+"""
+
+# $Id: ftp_stat.py,v 1.1 2003/03/16 19:20:54 schwa Exp $
+
+import sys
+
+
+if sys.version_info[:2] >= (2, 2):
+    _StatBase = tuple
+else:
+    import UserTuple
+    _StatBase = UserTuple.UserTuple
+
+
+class _Stat(_StatBase):
+    """
+    Support class resembling a tuple like that which is returned
+    from `os.(l)stat`.
+    """
+
+    _index_mapping = {
+      'st_mode':  0, 'st_ino':   1, 'st_dev':    2, 'st_nlink':    3,
+      'st_uid':   4, 'st_gid':   5, 'st_size':   6, 'st_atime':    7,
+      'st_mtime': 8, 'st_ctime': 9, '_st_name': 10, '_st_target': 11}
+
+    def __getattr__(self, attr_name):
+        if self._index_mapping.has_key(attr_name):
+            return self[ self._index_mapping[attr_name] ]
+        else:
+            raise AttributeError("'_Stat' object has no attribute '%s'" %
+                                 attr_name)
+
+
+# Unix format
+# total 14
+# drwxr-sr-x   2 45854    200           512 May  4  2000 chemeng
+# drwxr-sr-x   2 45854    200           512 Jan  3 17:17 download
+# drwxr-sr-x   2 45854    200           512 Jul 30 17:14 image
+# -rw-r--r--   1 45854    200          4604 Jan 19 23:11 index.html
+# drwxr-sr-x   2 45854    200           512 May 29  2000 os2
+# lrwxrwxrwx   2 45854    200           512 May 29  2000 osup -> ../os2
+# drwxr-sr-x   2 45854    200           512 May 25  2000 publications
+# drwxr-sr-x   2 45854    200           512 Jan 20 16:12 python
+# drwxr-sr-x   6 45854    200           512 Sep 20  1999 scios2
+
+# Microsoft ROBIN FTP server
+# 07-04-01  12:57PM       <DIR>          SharePoint_Launch
+# 11-12-01  04:38PM       <DIR>          Solution Sales
+# 06-27-01  01:53PM       <DIR>          SPPS
+# 01-08-02  01:32PM       <DIR>          technet
+# 07-27-01  11:16AM       <DIR>          Test
+# 10-23-01  06:49PM       <DIR>          Wernerd
+# 10-23-01  03:25PM       <DIR>          WindowsXP
+# 12-07-01  02:05PM       <DIR>          XPLaunch
+# 07-17-00  02:08PM             12266720 digidash.exe
+# 07-17-00  02:08PM                89264 O2KKeys.exe
+
