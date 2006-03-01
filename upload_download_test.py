0c0caad3332c2e012eed9a2b6e43a15ceb5979f5
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2006-03-01 12:18:18 +0000

    Return actually entered login data.
    --HG--
    extra : convert_revision : svn%3A778c30c8-61e0-0310-89d4-fe2f97a467b2/trunk%40524

--- upload_download_test.py
+++ upload_download_test.py
@@ -45,8 +45,7 @@ def login_data():
     remote_host = raw_input("Host: ")
     user = raw_input("User name: ")
     password = getpass.getpass("Password: ")
-    return "localhost", 'ftptest', 'd605581757de5eb56d568a4419f4126e'
-    #return remote_host, user, password
+    return remote_host, user, password
 
 def test_data():
     """Return a pseudo-random string of length between 0 and 5120."""
