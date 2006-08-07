f335fc7bacf0dda58146e240a18086b21d7e38cf
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2006-08-07 21:42:41 +0000

    Included the word "remote" in two error messages to make the error condition more explicit (idea from Alexander Michael).
    --HG--
    extra : convert_revision : svn%3A778c30c8-61e0-0310-89d4-fe2f97a467b2/trunk%40548

--- ftputil.py
+++ ftputil.py
@@ -240,8 +240,8 @@ class FTPHost:
         except ftp_error.PermanentError:
             # similarly to a failed `file` in a local filesystem, we
             #  raise an `IOError`, not an `OSError`
-            raise ftp_error.FTPIOError("directory '%s' doesn't exist or "
-                  "has insufficient access rights" % effective_dir)
+            raise ftp_error.FTPIOError("remote directory '%s' doesn't exist "
+                  "or has insufficient access rights" % effective_dir)
         host._file._open(effective_file, mode)
         return host._file
 
