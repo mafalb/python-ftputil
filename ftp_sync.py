5eecee76816d115168303fcb97d71f18b12a09fd
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2007-06-03 18:43:08 +0000

    Refactored syncing code: Make a method `sync` which handles both directory trees and individual files.
    If I don't use this for the upload/download methods, simplify the
    code back to its previous state.
    
    --HG--
    extra : convert_revision : svn%3A778c30c8-61e0-0310-89d4-fe2f97a467b2/trunk%40712

--- ftp_sync.py
+++ ftp_sync.py
@@ -52,7 +52,7 @@ import ftp_error
 __all__ = ['FTPHost', 'LocalHost', 'Syncer']
 
 
-# 64 KB
+# used for copying file objects; value is 64 KB
 CHUNK_SIZE = 64*1024
 
 
@@ -65,6 +65,14 @@ class LocalHost(object):
         # this is the built-in `open` function, not `os.open`!
         return open(path, mode)
 
+    def time_shift(self):
+        """
+        Return the time shift value (see methods `set_time_shift`
+        and `time_shift` in class `FTPHost` for a definition). By
+        definition, the value is zero for local file systems.
+        """
+        return 0.0
+
     def __getattr__(self, attr):
         return getattr(os, attr)
 
@@ -73,7 +81,7 @@ class Syncer(object):
     def __init__(self, source, target):
         """
         Init the `FTPSyncer` instance.
-        
+
         Each of `source` and `target` is either an `FTPHost` or a
         `LocalHost` object. The source and target directories, resp.
         have to be set with the `chdir` command before passing them
@@ -100,6 +108,9 @@ class Syncer(object):
             self._target.mkdir(target_dir)
 
     def _sync_file(self, source_file, target_file):
+        #XXX this duplicates code from `FTPHost._copyfileobj`; maybe
+        #  implement the upload and download methods in terms of
+        #  `_sync_file`, or maybe not?
         #TODO handle `IOError`s
         #TODO handle conditional copy
         #TODO handle setting of target mtime according to source mtime
@@ -115,10 +126,10 @@ class Syncer(object):
         finally:
             source.close()
 
-    def sync(self, source_dir, target_dir):
+    def _sync_tree(self, source_dir, target_dir):
         """
-        Synchronize the source and the target by updating the target
-        to match the source as far as possible.
+        Synchronize the source and the target directory tree by
+        updating the target to match the source as far as possible.
 
         Current limitations:
         - _don't_ delete items which are on the target path but not on the
@@ -128,8 +139,6 @@ class Syncer(object):
         - all files are copied in binary mode, never in ASCII/text mode
         - incomplete error handling
         """
-        source_dir = self._source.path.abspath(source_dir)
-        target_dir = self._target.path.abspath(target_dir)
         self._mkdir(target_dir)
         for dirpath, dirnames, filenames in self._source.walk(source_dir):
             for dirname in dirnames:
@@ -141,4 +150,20 @@ class Syncer(object):
                 source_file = self._source.path.join(dirpath, filename)
                 target_file = source_file.replace(source_dir, target_dir, 1)
                 self._sync_file(source_file, target_file)
+
+    def sync(self, source_path, target_path):
+        """
+        Synchronize `source_path` and `target_path` (both are strings,
+        each denoting a directory or file path), i. e. update the
+        target path so that it's a copy of the source path.
+
+        This method handles both directory trees and single files.
+        """
+        #TODO handle missing intermediate directories
+        source_path = self._source.path.abspath(source_path)
+        target_path = self._target.path.abspath(target_path)
+        if self._source.path.isfile(source_path):
+            self._sync_file(source_path, target_path)
+        else:
+            self._sync_tree(source_path, target_path)
 
