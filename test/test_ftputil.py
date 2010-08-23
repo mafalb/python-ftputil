7ec58876888693d38349342abdb630313c4c7059
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2010-08-23 23:13:29 +0200

    Moved timestamp comparison test to `test_file_transfer.py`.

--- test/test_ftputil.py
+++ test/test_ftputil.py
@@ -208,61 +208,6 @@ class TestRecursiveListingForDotAsPath(unittest.TestCase):
         host.close()
 
 
-class MockFile(object):
-    """Class compatible with `LocalFile` and `RemoteFile`."""
-
-    def __init__(self, mtime, mtime_precision):
-        self._mtime = mtime
-        self._mtime_precision = mtime_precision
-
-    def mtime(self):
-        return self._mtime
-
-    def mtime_precision(self):
-        return self._mtime_precision
-
-
-class TestTimestampComparison(unittest.TestCase):
-
-    def test_source_is_newer_than_target(self):
-        """Test whether the source is newer than the target, i. e.
-        if the file should be transferred."""
-        # Define some precisions.
-        second = 1.0
-        minute = 60.0 * second
-        hour = 60 * minute
-        day = 24 * hour
-        # Define input arguments; modification datetimes are in seconds.
-        #  Fields are source datetime/precision, target datetime/precision,
-        #  expected comparison result.
-        file_data = [
-          # Non-overlapping modification datetimes/precisions
-          (1000.0, second, 900.0, second, True),
-          (900.0, second, 1000.0, second, False),
-          # Equal modification datetimes/precisions (if in doubt, transfer)
-          (1000.0, second, 1000.0, second, True),
-          # Just touching intervals
-          (1000.0, second, 1000.0+second, minute, True),
-          (1000.0+second, minute, 1000.0, second, True),
-          # Other overlapping intervals
-          (10000.0-0.5*hour, hour, 10000.0, day, True),
-          (10000.0+0.5*hour, hour, 10000.0, day, True),
-          (10000.0+0.2*hour, 0.2*hour, 10000.0, hour, True),
-          (10000.0-0.2*hour, 2*hour, 10000.0, hour, True),
-        ]
-        for (source_mtime, source_mtime_precision,
-             target_mtime, target_mtime_precision,
-             expected_result) in file_data:
-            # print (source_mtime, source_mtime_precision,
-            #        target_mtime, target_mtime_precision,
-            #        expected_result)
-            source_file = MockFile(source_mtime, source_mtime_precision)
-            target_file = MockFile(target_mtime, target_mtime_precision)
-            result = file_transfer.source_is_newer_than_target(source_file, 
-                                                               target_file)
-            self.assertEqual(result, expected_result)
-
-
 class TestUploadAndDownload(unittest.TestCase):
     """Test ASCII upload and binary download as examples."""
 
