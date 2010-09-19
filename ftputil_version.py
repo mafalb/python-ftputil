a1d6d84f92bb3343902d729187b120d887426740
Stefan Schwarzer <sschwarzer@sschwarzer.net>
2010-09-19 12:05:07 +0200

    Updated version information.

--- ftputil_version.py
+++ ftputil_version.py
@@ -5,7 +5,7 @@ import sys
 
 
 # ftputil version number; substituted by `make patch`
-__version__ = '2.5dev'
+__version__ = '2.5b'
 
 _ftputil_version = __version__
 _python_version = sys.version.split()[0]
